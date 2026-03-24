06fe583ce8f802a596c7b3cfbe62393fb645fb20
unknown <ganganirijans@gmail.com>
2026-03-24 11:27:45 +0530

    fix: logout + oauth reload

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login"         element={<Login />} />
+          <Route path="/"         element={<Login />} />
           <Route path="/signup"        element={<Signup />} />
           <Route path="/auth/callback" element={<AuthCallback />} />
           <Route path="/share/:id"              element={<SharedBoard />} />
