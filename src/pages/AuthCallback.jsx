8e6a3cda727aac6c8a5d72d3317640bbd1e662e1
unknown <ganganirijans@gmail.com>
2026-03-24 14:58:56 +0530

    fix: don't redirect to login

--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabase";
 import toast from "react-hot-toast";
